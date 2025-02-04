2c3f3f536d89a4261c829b8fe93cdd9d13806da5
Khande <2374872@cognizant.com>
2025-02-04 23:36:41 +0530

    added the stylesheet link to other pages too to include in the layout and also added the manage-users functionality for admin...

--- src/main/java/com/tk/bharat_trains/thymeleaf/controller/AdminViewController.java
+++ src/main/java/com/tk/bharat_trains/thymeleaf/controller/AdminViewController.java
@@ -1,16 +1,36 @@
 package com.tk.bharat_trains.thymeleaf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tk.bharat_trains.service.UserService;
 
 @Controller
 @RequestMapping("/bharattrains/admin")
 public class AdminViewController {
 	
+	@Autowired
+	UserService userService;
 	
 	@GetMapping("/dashboard")
 	public String showAdminDashboard() {
-		return "admin-dashboard";
+		return "/admin/admin-dashboard";
+	}
+	
+	@GetMapping("/users")
+	public String showUsers(Model model) {
+		model.addAttribute("users", userService.getUsers());
+		return "admin/manage-users";
+	}
+	
+	@PostMapping("/deleteUser")
+	public String deleteUser(@RequestParam("userId") int userId) {
+		userService.deleteUser(userId);
+		return "redirect:/bharattrains/admin/users";
 	}
 }
